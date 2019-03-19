01ab63a4119c140f9469721c5494a40211b9e82c
opg.li.taojun <li.taojun@opg.cn>
2019-03-19 18:33:46 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Student from '@/components/Student'
+import Elementtab from '@/components/Elementtab'
+import home from '@/components/home'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/ff',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      children: [
+        {
+          path: 'bb',
+          name: 'HelloWorld',
+          component: HelloWorld
+        }]
+    },
+    {
+      path: '/test',
+      name: 'Elementtab',
+      component: Elementtab,
+      children: [
+        {
+          path: 'fff',
+          name: 'Elementtab',
+          component: Elementtab
+        }]
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home,
+      children: [
+        {
+          path: 'fff',
+          name: 'home',
+          component: home
+        }]
+    },
+    {
+      path: '/student',
+      name: 'Student',
+      component: Student,
+      children: [
+        {
+          path: 'fff',
+          name: 'Student',
+          component: Student
+        }]
+    }
+  ]
+})
